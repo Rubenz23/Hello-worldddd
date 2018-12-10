6ed696410c6eef28c0276eafa168fe31c573541b
Rubén <rubensmaterazzi@gmail.com>
2018-12-10 19:27:52 +0100

    Initial commit

---
+++ Assets/Bruum.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bruum : MonoBehaviour {
+
+    int cuenta = 0;
+    float velocidad = 0.4f;
+    public KeyCode tecla = KeyCode.F1;
+    public Text marcador;
+    
+    
+	// Update is called once per frame
+	void Update () {
+        
+       if (cuenta>75) { }
+       else {
+            if (Input.GetKeyDown(tecla))
+            {
+                this.transform.Translate(new Vector3(velocidad, 0, 0));
+
+                cuenta++;
+
+                marcador.text = "" + cuenta;
+
+            }
+           
+        }
+       
+    }
+}
+
---
+++ Assets/braaaam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class braaaam : MonoBehaviour
+{
+
+    int cuenta = 0;
+    float velocidad = 0.4f;
+    public KeyCode tecla = KeyCode.F1;
+    public Text marcador;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (cuenta > 75) { }
+        else
+        {
+            if (Input.GetKeyDown(tecla))
+            {
+                this.transform.Translate(new Vector3(-velocidad, 0, 0));
+                cuenta = cuenta + 1;
+
+                marcador.text = "" + cuenta;
+            }
+        }
+
+    }
+}
